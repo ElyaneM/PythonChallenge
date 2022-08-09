95a912564888291914735c8408fce3d5cc163197
Elyane Mehiyeva <elyamehiyeva@gmail.com>
2022-08-10 00:11:53 +0400

    ChallengeDay_1

---
+++ ChallengeDay_1.py
@@ -0,0 +1,99 @@
+"""
+    ChallengeDay_1 
+"""
+
+'''
+    Bura da
+'''
+
+print(3+1)       # Result : 4 Toplama
+print(4*5)       # Result : 20 Vurma
+print(6-5)       # Result : 1 Cixma
+print(30/6)      # Result : 5 Bolme
+
+
+print(2.1 + 3.1) # Result : 5.2
+
+
+a = 8
+b = 10
+print(a+b)        # Result : 18
+print(8 + 10)     # Result : 18
+
+print(a - b)      # Result : -2
+
+mushvig = 20
+elyane = 21
+print(mushvig)    # Result : 20
+
+print(mushvig + elyane) # R : 41
+
+mushvig = 19
+
+print(mushvig + elyane) # R : 40 
+
+elyane = 20
+print(mushvig + elyane) # R : 39
+print(mushvig - elyane) # R : -1
+print(mushvig * elyane) # R : 380
+print(mushvig / elyane) # R : 0.95
+mushvig_shukurov = 20
+elyane_shukurova = 21 # snake case
+mushvigShukurov = 20
+elyaneShukurova = 20 # camel Case
+ElyaneShukurova = 20
+MushvigShukurov = 21 # Paskal Case
+
+
+
+pi = 3.14
+radius = 5
+cevre = pi * radius *2 # 2pr
+print(cevre) # R : 31.4
+
+
+a = 5
+b = 6
+
+a,b = b,a
+print(a) # a = 6 oldu b=5
+a += 1
+print(a) # a = 7
+a = a + 1
+print(a) # a = 8
+b *= 4
+print(b) # R : 20 Bunlar yorum setridir
+
+
+
+
+
+# Ders 12 Bitdi ------------------------
+
+a = 333
+
+b = 5
+
+print(a/b)
+print(a//b)
+
+print(10%3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
