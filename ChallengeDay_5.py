696708ee59b276782b4ec6087a7f9f099d060660
Elyane Mehiyeva <elyamehiyeva@gmail.com>
2022-08-17 05:03:23 +0400

    ChallengeDay_5

---
+++ ChallengeDay_5.py
@@ -0,0 +1,91 @@
+"""
+ ChallengeDay_5
+
+"""
+
+# Ders 57-------------------------------------------
+
+# from cgitb import text
+# import math
+# from random import random
+# from time import time
+
+# #from .ChallengeDay_4 import factorial # bunu sildim oldu heeeey ay heyatim
+
+# print(dir(math))
+
+# # -------------------------------
+
+# print(math.factorial(5))
+
+# print(math.floor(2.7))    # 2
+
+# print(math.ceil(2.7))     # 3
+
+# # -------------------------------
+
+# import math as cebr
+
+# print(cebr.ceil(5.2))    # 6
+
+# # -------------------------------
+
+# # from math import*
+
+# # print(factorial(8))
+
+# # -------------------------------
+
+# from math import floor,ceil
+
+# print(floor(5.6))  # 5
+# print(ceil(5.2))   # 6
+# # print(factorial(5)) # error
+
+
+# # Ders 57 bitdi ------------------------------------
+
+# # Ders 59-------------------------------------------
+
+# import random
+# import time
+# ran_eded = random.randint(1,20) 
+# texmin_haqqi = 5
+# while True:
+
+#     texmin = int(input("Texmininiz : "))
+
+#     if(texmin<ran_eded):
+#         time.sleep(1)
+#         print("Daha yuksek reqem de")
+#         texmin_haqqi -= 1
+#     elif(texmin>ran_eded):
+#         time.sleep(1)
+#         print("Daha asagi eded deyin ")
+#         texmin_haqqi -= 1
+#     else:
+#         time.sleep(1)
+#         print("Tebrikler",ran_eded)
+#         break
+#     if(texmin_haqqi == 0):
+#         print("Texmin haqqiniz bitdi")
+#         print("Reqem",ran_eded)
+#         break
+
+# Ders 59 bitdi ------------------------------------
+
+# """
+#     Time Modulu
+# """
+
+# import time 
+
+# print("Program basladilir zehmet olmasa 5 saniye gozleyin")
+
+# time.sleep(5) # saniye dayanir
+
+# print("Program basladildi")
+# -----------------------------------------------------------
+
+
+# davam et ugurlar tamam
